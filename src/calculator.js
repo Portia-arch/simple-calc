65eba0186dc0a482264478a30f5c766da0f1bcb2
Portia-arch <55884163+Portia-arch@users.noreply.github.com>
2019-12-11 10:37:33 +0200

    updated simple calculator

--- src/calculator.js
+++ src/calculator.js
@@ -1,69 +1,21 @@
-class Calculator {
-    constructor() {
-        this.lastNum = 0;
-        this.slot = [];
+function addFunction(...args) {
+    let sum = 0;
+
+    for (i = 0; i < args.length; i++) {
+        sum = sum + args[i];
     }
+    return sum;
+}
+console.log(addFunction(1, 2, 3, 4));
 
-    //add numbers
-    add(...args) {
-        let sum = 0; 
-
-        args.forEach(num => {
-            if (isNaN(num)){
-                num = this.string_slot(num)
-            }
-            sum += num;
-        });
-            
-        this.lastNum = sum;
-        this.slot.push(sum);
-
-        return sum;
-    }
-    
-    //check for any keywords
-    string_slot(string) {
-        if (string == "Last") {
-            return this.last();
-        }
-
-        //return the last value of the string
-        if (string.includes('slot_')) {
-            let slot_num = string[string.length-1];
-            return this.slot[slot_num-1];
-        }
-
-        return 0;
-    }
-
-    multiply(...args) {
+function multiply(...args) {
     let quantity = 1;
 
-        args.forEach(num => {
-            if (num == "Last") {
-                num = this.last()
-            }
-            quantity *= num;
-        }); {
-        
-        }
-
-        this.lastNum = quantity;
-        this.slot.push(quantity)
-        return quantity;
+    for (i = 0; i < args.length; i++) {
+        quantity = quantity * args[i];
     }
-    //return last
-    last() {
-        return this.lastNum;
-    }
-
-    set_slot(num) {
-        this.lastNum = this.slot[num -1];
-    }
-
-    get_slot(num) {
-        return this.slot[num -1];
-    }
+    return quantity;
 }
+console.log(multiply(1, 2, 3, 4));
 
-module.exports = {Calculator}
+    module.exports = { addFunction, multiply}
